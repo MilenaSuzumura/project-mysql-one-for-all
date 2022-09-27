0e746bf1991ef79aeb4c08c2e850ad487d61b47e
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-09-27 19:23:17 -0300

    ajeitei o sql

--- desafio1.sql
+++ desafio1.sql
@@ -2,25 +2,25 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
 
-CREATE TABLE SpotifyClone.tabela1(
+CREATE TABLE SpotifyClone.users(
     coluna1 tipo restricoes,
     coluna2 tipo restricoes,
     colunaN tipo restricoes,
 ) engine = InnoDB;
 
-CREATE TABLE SpotifyClone.tabela2(
+CREATE TABLE SpotifyClone.plan(
     coluna1 tipo restricoes,
     coluna2 tipo restricoes,
     colunaN tipo restricoes,
 ) engine = InnoDB;
 
-INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
+INSERT INTO SpotifyClone.users (coluna1, coluna2)
 VALUES
   ('exemplo de dados 1', 'exemplo de dados A'),
   ('exemplo de dados 2', 'exemplo de dados B'),
   ('exemplo de dados 3', 'exemplo de dados C');
 
-INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
+INSERT INTO SpotifyClone.plan (coluna1, coluna2)
 VALUES
   ('exemplo de dados 1', 'exemplo de dados X'),
   ('exemplo de dados 2', 'exemplo de dados Y');
