7d103cd84ce2a45cd6f17590f6fa9f003d8db036
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-09-28 18:12:41 -0300

    fiz o following e o reproduction

--- desafio1.sql
+++ desafio1.sql
@@ -13,6 +13,13 @@ CREATE TABLE SpotifyClone.artist(
     artist_name VARCHAR(30) NOT NULL,
 ) engine = InnoDB;
 
+CREATE TABLE SpotifyClone.album(
+    album_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    album_name VARCHAR(30) NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artist (artist_id),
+) engine = InnoDB;
+
 CREATE TABLE SpotifyClone.song(
     song_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     song_name VARCHAR(30) NOT NULL,
@@ -30,27 +37,24 @@ CREATE TABLE SpotifyClone.users(
     users_age INT NOT NULL,
     plan_id INT NOT NULL,
     users_plan_date DATE NOT NULL,
-    plan_value FLOAT NOT NULL,
     FOREIGN KEY (plan_id) REFERENCES plan (plan_id),
-    FOREIGN KEY (plan_value) REFERENCES plan (plan_value),
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.reproduction(
-    coluna1 tipo restricoes,
-    coluna2 tipo restricoes,
-    colunaN tipo restricoes,
+    reproduction_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    reproduction_date DATE NOT NULL,
+    song_id INT NOT NULL,
+    users_id INT NOT NULL,
+    FOREIGN KEY (song_id) REFERENCES song (song_id),
+    FOREIGN KEY (users_id) REFERENCES users (users_id),
 ) engine = InnoDB;
 
 CREATE TABLE SpotifyClone.following(
-    coluna1 tipo restricoes,
-    coluna2 tipo restricoes,
-    colunaN tipo restricoes,
-) engine = InnoDB;
-
-CREATE TABLE SpotifyClone.album(
-    coluna1 tipo restricoes,
-    coluna2 tipo restricoes,
-    colunaN tipo restricoes,
+    following_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    artist_id INT NOT NULL,
+    users_id INT NOT NULL,
+    FOREIGN KEY (artist_id) references artist (artist_id),
+    FOREIGN KEY (users_id) references users (users_id)
 ) engine = InnoDB;
 
 INSERT INTO SpotifyClone.plan (plan_name, plan_value)
@@ -69,25 +73,31 @@ VALUES
   ('Blind Guardian'),
   ('Nina Simone');
 
-INSERT INTO SpotifyClone.album (coluna1, coluna2)
+INSERT INTO SpotifyClone.album (album_name, artist_id)
 VALUES
-  ('exemplo de dados 1', 'exemplo de dados X'),
-  ('exemplo de dados 2', 'exemplo de dados Y');
+  ('Renaissance', 1),
+  ('Jazz', 2),
+  ('Hot Space', 2),
+  ('Falso Brilhante', 3),
+  ('Vento de Maio', 3),
+  ('QVVJFA?', 4),
+  ('Somewhere Far Beyond', 5),
+  ('I Put A Spell On You', 6);
 
-INSERT INTO SpotifyClone.song (song_name, song_duration, song_years)
+INSERT INTO SpotifyClone.song (song_name, song_duration, song_years, artist_id, album_id)
 VALUES
-  ('BREAK MY SOUL', 279, 2022),
-  ('VIRGOS GROOVE', 369, 2022),
-  ('ALIEN SUPERSTAR', 116, 2022),
-  ('Dont Stop Me Now', 203, 1978),
-  ('Under Pressure', 152, 1982),
-  ('Como Nossos Pais', 105, 1998),
-  ('O Medo de Amar é o Medo de Ser Livre', 207, 2001),
-  ('Samba em Paris', 267, 2003),
-  ('The Bards Song', 244, 2007),
-  ('Feeling Good', 100, 2012);
+  ('BREAK MY SOUL', 279, 2022, 1, 1),
+  ('VIRGOS GROOVE', 369, 2022, 1, 1),
+  ('ALIEN SUPERSTAR', 116, 2022, 1, 1),
+  ('Dont Stop Me Now', 203, 1978, 2, 2),
+  ('Under Pressure', 152, 1982, 2, 3),
+  ('Como Nossos Pais', 105, 1998, 3, 4),
+  ('O Medo de Amar é o Medo de Ser Livre', 207, 2001, 3, 5),
+  ('Samba em Paris', 267, 2003, 4, 6),
+  ('The Bards Song', 244, 2007, 5, 7),
+  ('Feeling Good', 100, 2012, 6, 8);
 
-INSERT INTO SpotifyClone.users (users_name, users_age)
+INSERT INTO SpotifyClone.users (users_name, users_age, users_plan_date, plan_id)
 VALUES
   ('Barbara Liskov', 82, '2019-10-20', 1),
   ('Robert Cecil Martin', 58, '2017-01-06', 1),
@@ -100,12 +110,38 @@ VALUES
   ('Judith Butler', 45, '2020-05-13',3),
   ('Jorge Amado', 58, '2017-02-17', 3);
 
-INSERT INTO SpotifyClone.reproduction (coluna1, coluna2)
+INSERT INTO SpotifyClone.reproduction (reproduction_date, song_id, users_id)
 VALUES
-  ('exemplo de dados 1', 'exemplo de dados X'),
-  ('exemplo de dados 2', 'exemplo de dados Y');
+  ('2022-02-28 10:45:55', 8, 1),
+  ('2020-05-02 05:30:35', 2, 1),
+  ('2020-03-06 11:22:33', 10, 1),
+  ('2022-08-05 08:05:17', 10, 2),
+  ('2020-01-02 07:40:33', 7, 2),
+  ('2020-11-13 16:55:13', 10, 3),
+  ('2020-12-05 18:38:30', 2, 3),
+  ('2021-08-15 17:10:10', 8, 4),
+  ('2022-01-09 01:44:33', 8, 5),
+  ('2020-08-06 15:23:43', 5, 5),
+  ('2017-01-24 00:31:17', 7, 6),
+  ('2017-10-12 12:35:20', 1, 6),
+  ('2011-12-15 22:30:49', 4, 7),
+  ('2012-03-17 14:56:41', 4, 8),
+  ('2022-02-24 21:14:22', 9, 9),
+  ('2015-12-13 08:30:22', 3, 10);
 
-INSERT INTO SpotifyClone.following (coluna1, coluna2)
+INSERT INTO SpotifyClone.following (artist_id, users_id)
 VALUES
-  ('exemplo de dados 1', 'exemplo de dados X'),
-  ('exemplo de dados 2', 'exemplo de dados Y');
+  (1, 1),
+  (2, 1),
+  (3, 1),
+  (1, 2),
+  (3, 2),
+  (2, 3),
+  (4, 4),
+  (5, 5),
+  (6, 5),
+  (6, 6),
+  (1, 6),
+  (6, 7),
+  (3, 9),
+  (2, 10);
